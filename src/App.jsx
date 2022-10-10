36b820f12b42ab5a5f1484408fe2c9c797115323
wheatScrub11 <xantares301@gmail.com>
2022-10-10 18:35:40 -0500

    Updated home path

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/">
+          <Route exact path="/">
             <Route
               index
               element={
